5bcb09b32082d06571c3862bd53d77f8472ba6d1
Richard Lewis <rl403@cam.ac.uk>
2015-02-09 21:21:46 +0000

    added ability to read sdf from file name

--- skchem/io.py
+++ skchem/io.py
@@ -3,12 +3,17 @@ import pandas as _pd
 
 def read_sdf(sdf_file, *args, **kwargs):
 
+    if type(sdf_file) is str:
+        sdf_file = open(sdf_file)
+
     ms = []
     idx = []
     props = set()
 
     mol_supp = _Chem.ForwardSDMolSupplier(sdf_file, *args, **kwargs)
     for m in mol_supp:
+        if m is None:
+            continue
         ms.append(m)
         idx.append(m.GetProp('_Name'))
         props = props.union(props, set(m.GetPropNames()))
@@ -25,12 +30,13 @@ def read_sdf(sdf_file, *args, **kwargs):
     for prop in props:
         df[prop] = df.structure.apply(lambda m: get_prop(m, prop))
 
+    df.index.name = 'Name'
+
     return df
 
 @classmethod
 def from_sdf(self, *args, **kwargs):
     return read_sdf(*args, **kwargs)
-
 
 #set on pandas dataframe
 _pd.DataFrame.from_sdf = from_sdf
