1dbcac5c6c7f5c665b1a0f0f148d5e773373cdd9
Richard Lewis <rl403@cam.ac.uk>
2015-02-25 13:58:51 +0000

    updated read_sdf with error checking

--- skchem/io.py
+++ skchem/io.py
@@ -1,5 +1,8 @@
 from rdkit import Chem as _Chem
+import skchem as _skc
 import pandas as _pd
+import json
+from collections import defaultdict
 
 def read_sdf(sdf_file, *args, **kwargs):
 
@@ -11,10 +14,11 @@ def read_sdf(sdf_file, *args, **kwargs):
     props = set()
 
     mol_supp = _Chem.ForwardSDMolSupplier(sdf_file, *args, **kwargs)
-    for m in mol_supp:
+    for i, m in enumerate(mol_supp):
         if m is None:
-            continue
-        ms.append(m)
+            # raise Value Error, like in the json module when no json is detected
+            raise ValueError('Molecule {} could not be decoded.'.format(i + 1))
+        ms.append(_skc.Mol(m))
         idx.append(m.GetProp('_Name'))
         props = props.union(props, set(m.GetPropNames()))
 
@@ -30,7 +34,7 @@ def read_sdf(sdf_file, *args, **kwargs):
     for prop in props:
         df[prop] = df.structure.apply(lambda m: get_prop(m, prop))
 
-    df.index.name = 'Name'
+    df.index.name = 'name'
 
     return df
 
@@ -42,15 +46,30 @@ def read_smiles(smiles_file, *args, **kwargs):
     df = _pd.read_csv(smiles_file, delimiter='\t', header=0)
 
     df.columns = [col if i > 0 else 'structure' for i, col in enumerate(df.columns)]
-    df['structure'] = df['structure'].apply(_Chem.MolFromSmiles)
+    df['structure'] = df['structure'].apply(_Chem.MolFromSmiles) #perhaps should raise errors for incorrectly parsed molecules here?
 
     return df
 
-def _json(m):
+_to_dict = _pd.DataFrame.to_dict
+_to_json = _pd.DataFrame.to_json
 
-    i = Chem.rdchem.Compute2DCoords(m)
-    conformer_2d = m.GetConformer(i)
-    conformer_2d
+def to_dict(self, *args, **kwargs):
+    kwargs = defaultdict(None, kwargs)
+    if kwargs['orient'] == 'chemdoodle':
+        #this may be wrong - may have to extract
+        return {'m': [m.to_dict(kind='chemdoodle')['m'][0] for m in self.structure]}
+    else:
+        return _to_dict(*args, **kwargs)
+
+def to_json(self, *args, **kwargs):
+    kwargs = defaultdict(None, kwargs)
+    if kwargs['orient'] == 'chemdoodle':
+        return json.dumps(self.to_dict(*args, **kwargs))
+    else:
+        return _to_dict(*args, **kwargs)
+
+_pd.DataFrame.to_dict = to_dict
+_pd.DataFrame.to_json = to_json
 
 @classmethod
 def _from_sdf(self, *args, **kwargs):
