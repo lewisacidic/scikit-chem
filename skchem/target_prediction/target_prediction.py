a6ba38a52e1527e9cc90cd14263b982ffc76c7ce
Richard Lewis <rl403@cam.ac.uk>
2015-02-25 14:06:08 +0000

    prototyped a target prediction class

---
+++ skchem/target_prediction/target_prediction.py
@@ -0,0 +1,50 @@
+import pandas as _pd
+from skchem import Mol as _Mol
+
+class TargetPredictionAlgorithm(object):
+
+    """abstract target prediction class, inherit from this for a target prediction algorithm"""
+
+    def predict(self, obj):
+        if type(obj) is _Mol:
+            return self._m_predict(obj)
+
+        if type(obj) in [_pd.Series, _pd.DataFrame]:
+            return self._df_predict(obj)
+
+    def predict_proba(self, obj):
+        if type(obj) is _Mol:
+            return self._m_predict_proba(obj)
+
+        if type(obj) in [_pd.Series, _pd.DataFrame]:
+            return self._df_predict_proba(obj)
+
+    def predict_log_proba(self, obj):
+        if type(obj) is _Mol:
+            return self._m_predict_log_proba(obj)
+
+        if type(obj) in [_pd.Series, _pd.DataFrame]:
+            return self._df_predict_log_proba(obj)
+
+    def _m_predict(self, m):
+        return _pd.Series([0])
+
+    def _m_predict_proba(self, m):
+        return _pd.Series([0])
+
+    def _m_predict_log_proba(self, m):
+        return _pd.Series([0])
+
+    def _df_predict(self, df):
+        return _pd.Series([0])
+
+    def _df_predict_proba(self, df):
+        return _pd.Series([0])
+
+    def _df_predict_proba(self, df):
+        return _pd.Series([0])
+
+    def __call__(self, obj):
+        self.predict(obj)
+
+s
