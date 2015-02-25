dd58e915f3eb73a4534838ee63ca91fbc466850a
Richard Lewis <rl403@cam.ac.uk>
2015-02-25 22:53:03 +0000

    added address to Point3D repr and added str method

--- skchem/core/Point3D.py
+++ skchem/core/Point3D.py
@@ -10,4 +10,12 @@ class Point3D(rdkit.Geometry.rdGeometry.Point3D, _ChemicalObject):
             return {"x": round(self.x), "y": round(self.y), "z": round(self.z)}
 
     def __repr__(self):
-        return '<{klass} coords="({x:.2f}, {y:.2f}, {z:.2f})">'.format(klass=self.__class__.__name__, x=self.x, y=self.y, z=self.z)
+        return '<{klass} coords="({x:.2f}, {y:.2f}, {z:.2f})" at {address}>'.format(
+            klass=self.__class__.__name__, 
+            x=self.x, 
+            y=self.y, 
+            z=self.z, 
+            address=hex(id(self)))
+
+    def __str__(self):
+        return '({x:.2f}, {y:.2f}, {z:.2f})'.format(x=self.x, y=self.y, z=self.z)
