ff77f2350d18a5c174d530fc86cb9980da12418c
Rich Lewis <rl403@cam.ac.uk>
2016-05-15 16:29:50 +0100

    added docstrings for fingerprints

--- skchem/descriptors/fingerprints.py
+++ skchem/descriptors/fingerprints.py
@@ -4,16 +4,15 @@
 # License: 3-clause BSD
 
 """
-skchem.descriptors.fingerprints
+## skchem.descriptors.fingerprints
 
 Fingerprinting classes and associated functions are defined.
-
 """
 
 from functools import wraps
 
 import pandas as pd
-from rdkit.Chem import DataStructs
+from rdkit.Chem import DataStructs, GetDistanceMatrix
 from rdkit.Chem.rdMolDescriptors import (GetMorganFingerprint,
                                          GetHashedMorganFingerprint,
                                          GetAtomPairFingerprint,
@@ -106,7 +105,7 @@ def skchemize(func, columns=None, *args, **kwargs):
     >>> from skchem.data import resource
     >>> df = skchem.read_sdf(resource('test_sdf', 'multi_molecule-simple.sdf'))
     >>> df.structure.apply(f)
-          0     1     2     3     4     5     6     7     8     9     ...   2038  \\
+          0     1     2     3     4     5     6     7     8     9     ...   2038
     name                                                              ...
     297      0     0     0     0     0     0     0     0     0     0  ...      0
     6324     0     0     0     0     0     0     0     0     0     0  ...      0
@@ -138,14 +137,36 @@ class Fingerprinter(object):
 
     """ Fingerprinter Base class. """
 
-    def __init__(self, name, func):
+    def __init__(self, func, name=None):
+
+        """ A generic fingerprinter.  Create with a function.
+
+        Args:
+            func (callable):
+                A fingerprinting function that takes an skchem.Mol argument, and
+                returns an iterable of values.
+            name (str):
+                The name of the fingerprints that are being calculated"""
+
         self.NAME = name
         self.func = func
 
     def __call__(self, obj):
+
+        """ Call the fingerprinter directly.
+
+        This is a shorthand for transform. """
+
         return self.transform(obj)
 
     def __add__(self, other):
+
+        """ Add fingerprinters together to create a fusion fingerprinter.
+
+        Fusion featurizers will transform molecules to series with all
+        features from all component featurizers.
+        """
+
         fpers = []
         for fper in (self, other):
             if isinstance(fper, FusionFingerprinter):
@@ -190,13 +211,19 @@ class FusionFingerprinter(Fingerprinter):
     def transform(self, obj):
 
         if isinstance(obj, skchem.Mol):
-            return pd.concat([fp.transform(obj) for fp in self.fingerprinters],  keys=[fp.NAME for fp in self.fingerprinters])
+            return pd.concat([fp.transform(obj) for fp in self.fingerprinters],
+                             keys=[fp.NAME for fp in self.fingerprinters])
 
         elif isinstance(obj, pd.DataFrame):
-            return pd.concat([fp.transform(obj) for fp in self.fingerprinters], keys=[fp.NAME for fp in self.fingerprinters], axis=1)
+            return pd.concat([fp.transform(obj) for fp in self.fingerprinters],
+                             keys=[fp.NAME for fp in self.fingerprinters],
+                             axis=1)
 
         elif isinstance(obj, pd.Series):
-            return pd.concat([fp.transform(obj.structure) for fp in self.fingerprinters],  keys=[fp.NAME for fp in self.fingerprinters], axis=1)
+            return pd.concat([fp.transform(obj.structure) \
+                                for fp in self.fingerprinters],
+                             keys=[fp.NAME for fp in self.fingerprinters],
+                             axis=1)
 
         else:
             raise NotImplementedError
@@ -211,17 +238,32 @@ class MorganFingerprinter(Fingerprinter):
 
     NAME = 'morgan'
 
-    def __init__(self, radius=2, n_feats=2048, as_bits=True,
+    def __init__(self, radius=2, n_feats=2048, as_bits=False,
                  use_features=False, use_bond_types=True, use_chirality=False):
 
         """
-        @param radius
-        @param n_feats
-        @param as_bits
-        @param use_features
-        @param use_bond_types
-        @param use_chirality
+        Args:
+            radius (int):
+                The maximum radius for atom environments.
+                Default is `2`.
+            n_feats (int):
+                The number of features to which to fold the fingerprint down.
+                For unfolded, use `-1`.
+                Default is `2048`.
+            as_bits (bool):
+                Whether to return bits (`True`) or counts (`False`).
+                Default is `True`.
+            use_features (bool):
+                Whether to use map atom types to generic features (FCFP analog).
+                Default is `False`.
+            use_bond_types (bool):
+                Whether to use bond types to differentiate environments.
+                Default is `False`.
+            use_chirality (bool):
+                Whether to use chirality to differentiate environments.
+                Default is `False`.
         """
+
         self.radius = radius
         self.n_feats = n_feats
         self.as_bits = as_bits
@@ -231,31 +273,99 @@ class MorganFingerprinter(Fingerprinter):
 
     def _transform(self, mol):
 
-        """
-        @param mol
+        """Private method to transform a skchem molecule.
 
-        @returns pd.Series
+        Use transform` for the public method, which genericizes the argument to
+        iterables of mols.
+
+        Args:
+            mol (skchem.Mol): Molecule to calculate fingerprint for.
+
+        Returns:
+            pandas.Series:
+                Fingerprint as a series.
         """
-        if self.n_feats == -1:
+
+        if self.n_feats < 0:
 
             res = GetMorganFingerprint(mol, self.radius,
                                        useFeatures=self.use_features,
                                        useBondTypes=self.use_bond_types,
-                                       useChirality=self.use_chirality)
+                                       useChirality=self.use_chirality).GetNonzeroElements()
+            idx = pd.Index(list(res.keys()), name='features')
+
         else:
             res = list(GetHashedMorganFingerprint(mol, self.radius,
                                         nBits=self.n_feats,
                                         useFeatures=self.use_features,
                                         useBondTypes=self.use_bond_types,
                                         useChirality=self.use_chirality))
+            idx = pd.Index(range(self.n_feats), name='features')
 
-        res = pd.Series(res, name=mol.name)
+        res = pd.Series(res, name=mol.name, index=idx).astype(int)
 
         if self.as_bits:
-            return (res > 0).astype(int)
-        else:
-            return res
+            res = (res > 0).astype(np.uint8) #smallest memory size
 
+        return res
+
+    def grad(self, mol):
+
+        """ Calculate the pseudo gradient with resepect to the atoms.
+
+        The pseudo gradient is the number of times the atom set that particular
+        bit.
+
+        Args:
+            mol (skchem.Mol):
+                The molecule for which to calculate the pseudo gradient.
+
+        Returns:
+            pandas.DataFrame:
+                Dataframe of pseudogradients, with columns corresponding to
+                atoms, and rows corresponding to features of the fingerprint.
+        """
+
+        cols = pd.Index(list(range(len(mol.atoms))), name='atoms')
+        dist = GetDistanceMatrix(mol)
+
+        info = {}
+
+        if self.n_feats < 0:
+
+            res = GetMorganFingerprint(mol, self.radius,
+                                       useFeatures=self.use_features,
+                                       useBondTypes=self.use_bond_types,
+                                       useChirality=self.use_chirality,
+                                       bitInfo=info).GetNonzeroElements()
+            idx_list = list(res.keys())
+            idx = pd.Index(idx_list, name='features')
+            grad = np.zeros((len(idx), len(cols)))
+            for bit in info:
+                for atom_idx, radius in info[bit]:
+                    grad[idx_list.index(bit)] += (dist <= radius)[atom_idx]
+
+        else:
+
+            res = list(GetHashedMorganFingerprint(mol, self.radius,
+                                        nBits=self.n_feats,
+                                        useFeatures=self.use_features,
+                                        useBondTypes=self.use_bond_types,
+                                        useChirality=self.use_chirality,
+                                        bitInfo=info))
+            idx = pd.Index(range(self.n_feats), name='features')
+            grad = np.zeros((len(idx), len(cols)))
+
+            for bit in info:
+                for atom_idx, radius in info[bit]:
+                    grad[bit] += (dist <= radius)[atom_idx]
+
+        grad = pd.DataFrame(grad, index=idx, columns=cols)
+
+        if self.as_bits:
+            grad = (grad > 0)
+
+        return grad.astype(int)
 
 class AtomPairFingerprinter(Fingerprinter):
 
@@ -263,7 +373,30 @@ class AtomPairFingerprinter(Fingerprinter):
 
     NAME = 'atom_pair'
 
-    def __init__(self, min_length=1, max_length=30, n_feats=2048, as_bits=False, use_chirality=False):
+    def __init__(self, min_length=1, max_length=30, n_feats=2048, as_bits=False,
+                 use_chirality=False):
+
+        """ Instantiate an atom pair fingerprinter.
+
+        Args:
+            min_length (int):
+                The minimum length of paths between pairs.
+                Default is `1`, i.e. pairs can be bonded together.
+            max_length (int):
+                The maximum length of paths between pairs.
+                Default is `30`.
+            n_feats (int):
+                The number of features to which to fold the fingerprint down.
+                For unfolded, use `-1`.
+                Default is `2048`.
+            as_bits (bool):
+                Whether to return bits (`True`) or counts (`False`).
+                Default is `False`.
+            use_chirality (bool):
+                Whether to use chirality to differentiate environments.
+                Default is `False`.
+        """
+
         self.min_length = min_length
         self.max_length = max_length
         self.n_feats = n_feats
@@ -272,10 +405,17 @@ class AtomPairFingerprinter(Fingerprinter):
 
     def _transform(self, mol):
 
-        """
-        @param molecules
+        """Private method to transform a skchem molecule.
 
-        @return pd.Series
+        Use transform` for the public method, which genericizes the argument to
+        iterables of mols.
+
+        Args:
+            mol (skchem.Mol): Molecule to calculate fingerprint for.
+
+        Returns:
+            pandas.Series:
+                Fingerprint as a series.
         """
 
         if self.n_feats == -1:
@@ -284,7 +424,8 @@ class AtomPairFingerprinter(Fingerprinter):
                                          maxLength=self.max_length,
                                          includeChirality=self.use_chirality)
         else:
-            res = list(GetHashedAtomPairFingerprint(mol, minLength=self.min_length,
+            res = list(GetHashedAtomPairFingerprint(mol,
+                                        minLength=self.min_length,
                                          maxLength=self.max_length,
                                          nBits=self.n_feats,
                                          includeChirality=self.use_chirality))
@@ -302,16 +443,34 @@ class TopologicalTorsionFingerprinter(Fingerprinter):
 
     def __init__(self, target_size=4, n_feats=2048, as_bits=False,
                  use_chirality=False):
-                 self.target_size = target_size
-                 self.n_feats = n_feats
-                 self.as_bits = as_bits
-                 self.use_chirality = use_chirality
+
+        """
+        Args:
+            target_size (int):
+                # TODO
+            n_feats (int):
+                The number of features to which to fold the fingerprint down.
+                For unfolded, use `-1`.
+                Default is `2048`.
+            as_bits (bool):
+                Whether to return bits (`True`) or counts (`False`).
+                Default is `False`.
+            use_chirality (bool):
+                Whether to use chirality to differentiate environments.
+                Default is `False`.
+        """
+
+        self.target_size = target_size
+        self.n_feats = n_feats
+        self.as_bits = as_bits
+        self.use_chirality = use_chirality
 
     def _transform(self, mol):
 
         if self.n_feats == -1:
 
-            res = GetTopologicalTorsionFingerprint(mol, targetSize=self.targetSize,
+            res = GetTopologicalTorsionFingerprint(mol,
+                                        targetSize=self.targetSize,
                                         includeChirality=self.use_chirality)
 
         else:
@@ -355,7 +514,7 @@ class ErGFingerprinter(Fingerprinter):
 
 class FeatureInvariantsFingerprinter(Fingerprinter):
 
-    """ Feature invariant fingerprints. """
+    """ Feature invariants fingerprints. """
 
     NAME = 'feat_inv'
 
@@ -368,8 +527,8 @@ class FeatureInvariantsFingerprinter(Fingerprinter):
 
 class ConnectivityInvariantsFingerprinter(Fingerprinter):
 
-    """ Connectiity invariant fingerprints """
-    
+    """ Connectivity invariants fingerprints """
+
     NAME = 'conn_inv'
 
     def __init__(self):
@@ -388,15 +547,43 @@ class RDKFingerprinter(Fingerprinter):
     def __init__(self, min_path=1, max_path=7, n_feats=2048, n_bits_per_hash=2,
                  use_hs=True, target_density=0.0, min_size=128,
                  branched_paths=True, use_bond_types=True):
-                 self.min_path = 1
-                 self.max_path = 7
-                 self.n_feats = 2048
-                 self.n_bits_per_hash = 2
-                 self.use_hs = True
-                 self.target_density = 0.0
-                 self.min_size = 128
-                 self.branched_paths = True
-                 self.use_bond_types = True
+
+        """ RDK fingerprints
+
+        # TODO
+
+        Args:
+            min_path (int):
+
+            max_path (int):
+
+            n_feats (int):
+                The number of features to which to fold the fingerprint down.
+                For unfolded, use `-1`.
+                Default is `2048`.
+
+            n_bits_per_hash (int)
+
+            use_hs (bool):
+
+            target_density (float):
+
+            min_size (int):
+
+            branched_paths (bool):
+
+            use_bond_types (bool):
+        """
+
+        self.min_path = 1
+        self.max_path = 7
+        self.n_feats = 2048
+        self.n_bits_per_hash = 2
+        self.use_hs = True
+        self.target_density = 0.0
+        self.min_size = 128
+        self.branched_paths = True
+        self.use_bond_types = True
 
     def _transform(self, mol):
 
