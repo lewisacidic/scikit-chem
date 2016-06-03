82f0abb2c07b1b531d6c4f9e0ec57e0fe4b68006
Rich Lewis <rl403@cam.ac.uk>
2016-06-03 14:30:48 +0100

    added some molecule tests

--- skchem/test/test_core/test_mol.py
+++ skchem/test/test_core/test_mol.py
@@ -1,4 +1,32 @@
 #! /usr/bin/env python
 #
-# Copyright (C) 2007-2009 Rich Lewis <rl403@cam.ac.uk>
+# Copyright (C) 2016 Rich Lewis <rl403@cam.ac.uk>
 # License: 3-clause BSD
+
+import skchem
+import pytest
+
+@pytest.fixture
+def m():
+    return skchem.Mol.from_smiles('CC')
+@pytest.fixture
+def m_c():
+    return skchem.Mol.from_smiles('CC')
+@pytest.fixture
+def m2():
+    return skchem.Mol.from_smiles('CCC')
+
+def test_smiles(m):
+    assert len(m.atoms) == 2
+    for a in m.atoms:
+        assert a.element == 'C'
+
+def test_substructure(m, m2):
+    assert m in m2
+    assert m2 not in m
+
+def test_equality(m, m_c):
+    assert m == m_c
+
+def test_binary(m):
+    assert m == skchem.Mol.from_binary(m.to_binary())
