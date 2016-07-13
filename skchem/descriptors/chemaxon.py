4d093fa005a576fc0868b73e82c96d8c0ac86364
Rich Lewis <rl403@cam.ac.uk>
2016-07-13 11:41:46 +0100

    updated the copyright to be for proper years!

--- skchem/descriptors/chemaxon.py
+++ skchem/descriptors/chemaxon.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 #
-# Copyright (C) 2015 Rich Lewis <rl403@cam.ac.uk>
+# Copyright (C) 2016 Rich Lewis <rl403@cam.ac.uk>
 # License: 3-clause BSD
 
 """
--- skchem/utils/string.py
+++ skchem/utils/string.py
@@ -1,9 +1,12 @@
 #! /usr/bin/env python
 #
-# Copyright (C) 2014-2016 Rich Lewis <rl403@cam.ac.uk>
+# Copyright (C) 2015-2016 Rich Lewis <rl403@cam.ac.uk>
 # License: 3-clause BSD
 
 import re
 
 def camel_to_snail(s):
     return re.sub('(?!^)([A-Z]+)', r'_\1', s).lower()
+
+def free_to_snail(s):
+    return s.replace(' ', '_').lower()
