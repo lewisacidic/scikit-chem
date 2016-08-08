827d1ca81bc185b35707086ab13a20b674cc9714
Rich Lewis <rl403@cam.ac.uk>
2016-08-08 16:49:33 +0100

    updated setup.py classifiers to remove unsupported python versions and upgrade to prealpha

--- setup.py
+++ setup.py
@@ -16,20 +16,17 @@ MAINTAINER_EMAIL = 'rl403@cam.ac.uk'
 URL = 'http://github.com/richlewis42/scikit-chem'
 LICENSE = 'new BSD'
 DOWNLOAD_URL = 'https://github.com/richlewis42/scikit-chem/archive/master.zip'
-CLASSIFIERS = [ 
-    'Development Status :: 1 - Planning',
+CLASSIFIERS = [
+    'Development Status:: 2 - Pre - Alpha',
     'Intended Audience :: Science/Research',
-    'Intended Audience :: Education',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Natural Language :: English',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Operating System :: Microsoft :: Windows',
     'Operating System :: POSIX',
     'Operating System :: Unix',
